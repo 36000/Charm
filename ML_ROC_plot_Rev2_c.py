48ce98976cdf9a6f1cd6c45c2a239268a063fe9f
John <jk23@att.net>
2017-06-14 12:27:42 -0700

    fix AUC

--- ML_ROC_plot_Rev2_c.py
+++ ML_ROC_plot_Rev2_c.py
@@ -5,7 +5,7 @@ tpr = np.genfromtxt('tpr.csv', delimiter=',')
 fpr = np.genfromtxt('fpr.csv', delimiter=',')
 
 AUC = np.trapz(tpr, x=fpr)
-st_AUC = "AUC = " + str('%.2f' % round(AUC, 2))
+st_AUCb = "AUCb = " + str('%.2f' % round(AUC, 2))
 
 pltRoc = plt.plot(fpr, tpr, linewidth=2, drawstyle='steps-post', color='blue')
 pltDiag = plt.plot([0,1],[0,1], 'r--')
@@ -14,11 +14,12 @@ tpr = np.genfromtxt('tprc.csv', delimiter=',')
 fpr = np.genfromtxt('fprc.csv', delimiter=',')
 
 AUC = np.trapz(tpr, x=fpr)
-st_AUC = "AUC = " + str('%.2f' % round(AUC, 2))
+st_AUCc = "AUCc = " + str('%.2f' % round(AUC, 2))
 
 pltRoc = plt.plot(fpr, tpr, linewidth=2, drawstyle='steps-post', color='green')
 pltDiag = plt.plot([0,1],[0,1], 'r--')
-plt.text(0.7,0.1, st_AUC, fontsize=17, weight=550)
+plt.text(0.7,0.2, st_AUCb, fontsize=17, weight=550)
+plt.text(0.7,0.1, st_AUCc, fontsize=17, weight=550)
 plt.xlim([0,1])
 plt.ylim([0,1])
 plt.xlabel('false positive rate (fpr)', fontsize=15)
